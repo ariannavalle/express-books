7b6746848f38b41e5f467f3e1190fd2943a8d12e
Arianna Valle <ariannamarbella@gmail.com>
2020-08-04 21:49:05 -0400

    Add "get" and "get by Id"

--- routes/book-routes/book.js
+++ routes/book-routes/book.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Book = require("../../models/book");
 
+router.get("/browse", (req,res,next)=>{
+	Book.find()
+	.then(booksFromDB => {
+		console.log(`List of books: ${booksFromDB}`)
+		res.render("book-views/listBooks", {books: booksFromDB})
+	})
+	.catch(err => console.log(`Error retrieving books: ${err}`))
+})
+
 router.get("/create", (req, res, next) => {
 	res.render("book-views/createBook");
 });
@@ -14,5 +23,15 @@ router.post("/create", (req, res, next) => {
 		})
 		.catch((err) => console.log(`Error creating book: ${err}`));
 });
+
+router.get('/:id', (req, res, next)=>{
+	Book.findById(req.params.id)
+	.then(bookFromDB => {
+		console.log("test:",bookFromDB)
+		res.render("book-views/bookDetails", {book : bookFromDB})
+	})
+	.catch(err => console.log(`Error retrieving book details: ${err}`))
+	
+})
 
 module.exports = router;
