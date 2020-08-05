c2618a31f6e7a8b6fd28d3b31ddbce374953c9af
Arianna Valle <ariannamarbella@gmail.com>
2020-08-04 21:48:07 -0400

    Add more fields to model

--- models/book.js
+++ models/book.js
@@ -4,7 +4,14 @@ const {Schema, model} = mongoose;
 const bookSchema = new Schema(
     {
         title: {type: String, required: true},
-        author: {type: [String]}
+        author: {type: [String]},
+        description: {type: String}, 
+        coverImg: {type: String, default:"https://memegenerator.net/img/images/16143029.jpg"},
+        year: {type: Number},
+        rating: {type: Number}
+    },
+    {
+        timestamps: true
     }
 )
 
