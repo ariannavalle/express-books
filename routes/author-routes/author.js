6b8b5ab64c5dd5dd4c1171984c14d6a2b5826c3f
Arianna Valle <ariannamarbella@gmail.com>
2020-08-14 22:04:36 -0400

    Start implementing two way binding

--- routes/author-routes/author.js
+++ routes/author-routes/author.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../../models/author");
+const Book = require("../../models/book")
 
 router.get("/browse", (req,res,next)=>{
 	Author.find()
@@ -28,17 +29,48 @@ router.get("/update/:id", (req, res, next) => {
 		const { id } = req.params;
 		Author.findById(id)
 		  .then(authorToEdit => {
-			res.render("author-views/editAuthor", authorToEdit);
+			Book.find()
+			.then((books) => {
+				const data = {
+					...authorToEdit,
+					books,
+				};
+				res.render("author-views/editAuthor", data);
+				console.log(data)
 		  })
-		  .catch(error => console.log(`Error while getting a single author for edit: ${error}`));
-	  });
+		  .catch(error => console.log(`Error getting books to edit: ${error}`));
+	  })
+	  .catch((err) =>
+	  console.log(`Error finding author in Database to edit: ${err}`)
+  );
+});
+
 
 router.post("/update/:id", (req, res, next) => {
     Author.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then((updatedAuthor) => {
+			Book.find({ _id: { $in: updatedAuthor.books } })
+                .then(async (booksArray) => {
+                    // console.log({ booksArray });
+
+                    // loop through each book in the array ov books that belongs to the cast member and add the author array from the book.
+                    await booksArray.forEach(async (book) => {
+                        if (
+                            !book.author.includes(req.params.id)
+                        ) {
+                            // save the book info in the database once the cast member id has been added the list of author in the book
+                            book.author.push(req.params.id);
+                            await book.save();
+                        }
+                    });
 			console.log({ updatedAuthor });
 			res.redirect(`/authors/${req.params.id}`)
-        })
+        }).catch((err) =>
+		console.log(
+			`Error getting books from cast member array: ${err}`
+		)
+	)
+})
         .catch((err) => console.log(`Error updating author: ${err}`));
 });
 
